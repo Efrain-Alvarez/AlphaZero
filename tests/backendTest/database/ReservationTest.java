42eb25e90584c11438941744aef642f1251b7358
Eric Torres <erictorres4@protonmail.com>
2023-05-17 11:22:30 -0700

    Add test code for Reservation class

---
+++ tests/backendTest/database/ReservationTest.java
@@ -0,0 +1,39 @@
+package backendTest.database;
+
+import backend.database.Reservation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ReservationTest {
+    @Test
+    void testDefaultConstructor() {
+        Reservation r = new Reservation();
+        Assertions.assertEquals("", r.getName());
+        Assertions.assertEquals("", r.getPhoneNumber());
+        Assertions.assertEquals(0, r.getPartySize());
+        Assertions.assertEquals(0, r.getPreorderItems().size());
+        Assertions.assertEquals(0, r.getSpecialRequests().size());
+    }
+
+    @Test
+    void testPreorderItems() {
+        // Test that adding preorder items works correctly
+        Reservation r = new Reservation();
+        Assertions.assertEquals(0, r.getPreorderItems().size());
+        r.addPreOrderItem("");
+        Assertions.assertEquals(1, r.getPreorderItems().size());
+        r.addPreOrderItem("");
+        Assertions.assertEquals(2, r.getPreorderItems().size());
+    }
+
+    @Test
+    void testSpecialRequests() {
+        // Test that adding preorder items works correctly
+        Reservation r = new Reservation();
+        Assertions.assertEquals(0, r.getSpecialRequests().size());
+        r.addSpecialRequest("");
+        Assertions.assertEquals(1, r.getSpecialRequests().size());
+        r.addSpecialRequest("");
+        Assertions.assertEquals(2, r.getSpecialRequests().size());
+    }
+}
