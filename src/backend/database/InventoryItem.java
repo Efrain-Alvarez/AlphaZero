9af07ef1de268ea7ee6d3ad04263d81b58c75a94
Eric Torres <erictorres4@protonmail.com>
2023-05-16 23:51:17 -0700

    Add docstrings for all InventoryItem methods

--- src/backend/database/InventoryItem.java
+++ src/backend/database/InventoryItem.java
@@ -13,6 +13,8 @@ public class InventoryItem {
     }
 
     /**
+     * Constructor for <code>InventoryItem</code> class. Note that any names are converted to lowercase
+     * for consistency.
      * @param name name of item
      * @param amount a positive integer indicating quantity of item on hand
      * @throws RuntimeException if the amount was negative
@@ -25,14 +27,25 @@ public class InventoryItem {
             throw new RuntimeException("Item amount " + amount + " was negative, must be positive");
     }
 
+    /**
+     * @return this item's name
+     */
     public String getItemName() {
         return itemName;
     }
 
+    /**
+     * @return the stored amount of this item on the database
+     */
     public int getItemCount() {
         return amount;
     }
 
+    /**
+     * Change the amount of this item.
+     * @param newAmount the new amount to set item to
+     * @throws RuntimeException if the new amount is a negative number
+     */
     public void setAmount(int newAmount) throws RuntimeException{
         if (newAmount < 0) {
             throw new RuntimeException("Item amount " + newAmount + " was negative, must be positive");
