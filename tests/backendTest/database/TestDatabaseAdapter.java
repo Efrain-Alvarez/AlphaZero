99fcfcdb3d41fbddffcba6d57cb310a194d7b953
Eric Torres <erictorres4@protonmail.com>
2023-05-17 14:56:00 -0700

    Only use one instance of DatabaseAdapter to run tests on

--- tests/backendTest/database/TestDatabaseAdapter.java
+++ tests/backendTest/database/TestDatabaseAdapter.java
@@ -2,9 +2,12 @@ package backendTest.database;
 
 import backend.database.DatabaseAdapter;
 import backend.database.Reservation;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
@@ -12,64 +15,61 @@ import java.time.format.DateTimeParseException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestDatabaseAdapter {
-    private static final String configPath = "/home/etorres/Programming/cs370/Project/repo/config.ini";
+    private static final String validConfigPath = "/home/etorres/Programming/cs370/Project/repo/config.ini";
+    private static DatabaseAdapter db;
+
+    @BeforeAll
+    static void beforeAll() throws SQLException, FileNotFoundException {
+        db = new DatabaseAdapter(validConfigPath);
+    }
+
+    @AfterAll
+    static void afterAll() throws Exception {
+        db.close();
+    }
 
     @Test
-    void testDatabaseConnection() throws Exception {
-        try (DatabaseAdapter db = new DatabaseAdapter(configPath)) {
-        }
+    void testInvalidPath() throws Exception {
         assertThrows(FileNotFoundException.class, () -> new DatabaseAdapter((".non/existent/path")));
     }
 
     // Make sure getInventory can run without error normally
     @Test
     void testGetInventoryItems() throws Exception {
-        try (DatabaseAdapter db = new DatabaseAdapter(configPath)) {
-            var items = db.getItems();
-        }
+        var items = db.getItems();
     }
 
     @Test
     void testAddDuplicateItem() throws Exception {
-        try (DatabaseAdapter db = new DatabaseAdapter(configPath)) {
-            db.addInventoryItem("Test Item", 5);
-            assertThrows(SQLIntegrityConstraintViolationException.class, () -> db.addInventoryItem("Test Item", 6));
-            db.deleteInventoryItem("Test Item");
-        }
+        db.addInventoryItem("Test Item", 5);
+        assertThrows(SQLIntegrityConstraintViolationException.class, () -> db.addInventoryItem("Test Item", 6));
+        db.deleteInventoryItem("Test Item");
     }
 
     @Test
     void testGetInventoryAmount() throws Exception {
-        try (DatabaseAdapter db = new DatabaseAdapter(configPath)) {
-            db.addInventoryItem("Test Item", 5);
-            assertEquals(5, db.getInventoryAmount("Test Item"));
-            db.deleteInventoryItem("Test Item");
-        }
+        db.addInventoryItem("Test Item", 5);
+        assertEquals(5, db.getInventoryAmount("Test Item"));
+        db.deleteInventoryItem("Test Item");
     }
 
     @Test
     void testGetReservations() throws Exception {
-        try (DatabaseAdapter db = new DatabaseAdapter(configPath)) {
-            var reservations = db.getReservations();
-        }
+        var reservations = db.getReservations();
     }
 
     @Test
     void testAddDeleteReservation() throws Exception {
-        try (DatabaseAdapter db = new DatabaseAdapter(configPath)) {
-            Reservation r = new Reservation("test name", "test number", LocalDateTime.now(), 1, 1);
-            db.addReservation(r);
-            db.deleteReservationByName(r.getName());
-        }
+        Reservation r = new Reservation("test name", "test number", LocalDateTime.now(), 1, 1);
+        db.addReservation(r);
+        db.deleteReservationByName(r.getName());
     }
 
     @Test
     void testDate() throws Exception {
-        try (DatabaseAdapter db = new DatabaseAdapter(configPath)) {
-            // parse valid date and time
-            assertDoesNotThrow(() -> db.parseDateAndTime("2023-05-17", "14", "00"));
-            //assertDoesNotThrow(() -> db.parseDateAndTime("05/17/23", "14", "00"));
-            assertThrows(DateTimeParseException.class, () -> db.parseDateAndTime("05/17/23", "0", "00"));
-        }
+        // parse valid date and time
+        assertDoesNotThrow(() -> db.parseDateAndTime("2023-05-17", "14", "00"));
+        //assertDoesNotThrow(() -> db.parseDateAndTime("05/17/23", "14", "00"));
+        assertThrows(DateTimeParseException.class, () -> db.parseDateAndTime("05/17/23", "0", "00"));
     }
 }
