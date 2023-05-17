f0400b4f63bbc24e8af00782d756e67aa48cec97
Eric Torres <erictorres4@protonmail.com>
2023-05-17 14:33:16 -0700

    Remove unnecessary tests

--- tests/backendTest/database/TestDatabaseAdapter.java
+++ tests/backendTest/database/TestDatabaseAdapter.java
@@ -9,37 +9,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TestDatabaseAdapter {
-    private static String configPath = "/home/etorres/Programming/cs370/Project/repo/config.ini";
-
-    @Test
-    void getInventoryAmount() {
-    }
-
-    @Test
-    void getItems() {
-    }
-
-    @Test
-    void addReservation() {
-    }
-
-    @Test
-    void getReservations() {
-    }
+    private static final String configPath = "/home/etorres/Programming/cs370/Project/repo/config.ini";
 
     @Test
     void testDatabaseConnection() throws Exception {
         try (DatabaseAdapter db = new DatabaseAdapter(configPath)) {
-            db.getReservations();
         }
     }
-
-    //@Test
-    //void testReservationCreationAndDeletion() throws Exception {
-    //    DatabaseAdapter db = new DatabaseAdapter(configPath);
-    //    Reservation r = new Reservation("Eric Torres", "5555555555", null, 3);
-    //    db.addReservation(r);
-    //}
 
     // Make sure getInventory can run without error normally
     @Test
